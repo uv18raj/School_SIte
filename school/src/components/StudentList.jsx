557a98fa1a07d63195da0c5629a3bc178dc7ffcf
uv18raj <utsavraj9131@gmail.com>
2024-12-29 22:50:15 +0530

    FIxed some issue

--- school/src/components/StudentList.jsx
+++ school/src/components/StudentList.jsx
@@ -70,7 +70,8 @@ const StudentList = () => {
               <td>Rs {student.totalAmountPaid}</td>
               <td>
                 {new Date(student.datePaid).toLocaleDateString('en-GB')}{" "}
-                {new Date(student.datePaid).toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour12: true })}
+                {new Date(new Date(student.datePaid).toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })).toLocaleTimeString('en-US', { hour12: true })}
+
               </td>
               <td>
                 <button
